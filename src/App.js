0c8fcbe357bf6ab7451747f1864d1fd1a8030bce
S <s@MacBook-Air-S.local>
2021-03-18 13:55:17 +0700

    minor edits

--- src/App.js
+++ src/App.js
@@ -5,62 +5,83 @@ import { useState, useEffect } from 'react';
 function App() {
 
   const initData = {
-    "RUB": {first: 0, second: 0, third: 0},
-    'USD': {first: 0, second: 0, third: 0},
-    'EUR': {first: 0, second: 0, third: 0},
-    'RUB/USD': {first: 0, second: 0, third: 0},
-    'RUB/EUR': {first: 0, second: 0, third: 0},
-    'EUR/USD': {first: 0, second: 0, third: 0}
+    "RUB/CUPCAKE": {first: 0, second: 0, third: 0, min: null},
+    'USD/CUPCAKE': {first: 0, second: 0, third: 0, min: null},
+    'EUR/CUPCAKE': {first: 0, second: 0, third: 0, min: null},
+    'RUB/USD': {first: 0, second: 0, third: 0, min: null},
+    'RUB/EUR': {first: 0, second: 0, third: 0, min: null},
+    'EUR/USD': {first: 0, second: 0, third: 0, min: null}
   };
 
-  const [data, setData] = useState(initData);
-  const [valueMin, setValueMin] = useState(data['RUB'].first);
-  
-  async function subscribe(url, market) {
-    console.log(market)
-    const response = await fetch(url);
-    
-   if (response.status !== 200) {
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      await subscribe(url, market);
-    } else {
-      const res = await response.json();
-      res.rates = {
-        ...res.rates,
-        'RUB/USD': res.rates.RUB / res.rates.USD,
-        'RUB/EUR': res.rates.RUB / res.rates.EUR,
-        'EUR/USD': res.rates.EUR / res.rates.USD
-      }
-
-      setData(data => {
-        let newData = {};
-        for (let key in data) {
-          newData[key] = {...data[key], [market]: res.rates[key]}
-        }
-        return newData
-      })
-     
-      await subscribe(url, market);
-    }
+  const initMin = {
+    "RUB/CUPCAKE":  null,
+    'USD/CUPCAKE':  null,
+    'EUR/CUPCAKE':  null,
+    'RUB/USD':  null,
+    'RUB/EUR':  null,
+    'EUR/USD':  null
   }
 
+  const [data, setData] = useState(initData);
+  const [valuesMin, setValuesMin] = useState(initMin);
+
   useEffect(() => {
+    async function subscribe(url, market) {
+      const response = await fetch(url);
+      
+     if (response.status !== 200) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await subscribe(url, market);
+      } else {
+        const result = await response.json();
+        const rates = {
+          "RUB/CUPCAKE": result.rates.RUB,
+          'USD/CUPCAKE': result.rates.USD,
+          'EUR/CUPCAKE': result.rates.EUR,
+          'RUB/USD': result.rates.RUB / result.rates.USD,
+          'RUB/EUR': result.rates.RUB / result.rates.EUR,
+          'EUR/USD': result.rates.EUR / result.rates.USD
+        }
+  
+        setData(data => {
+          let newData = {};
+          for (let key in data) {
+            newData[key] = {
+              ...data[key],
+              [market]: rates[key]
+            }
+          }
+          return newData;
+        })
+       
+        await subscribe(url, market);
+      }
+    }
+
     subscribe('/api/v1/first/poll', 'first');
     subscribe('/api/v1/second/poll', 'second');
     subscribe('/api/v1/third/poll', 'third');
-  }, [subscribe]);
+
+  }, []);
 
   useEffect(() => {
-    console.log(data)
+    
   }, [data])
 
   return (
     <>
     <div className="App">
-        {Object.values(data).map(item => <div className='row'>
-          <div className='value'>{+item['first'].toFixed(3)}</div>
-          <div className='value'>{+item['second'].toFixed(3)}</div>
-          <div className='value'>{+item['third'].toFixed(3)}</div>
+      <div className='row'>
+        <div className='title'>Pair name / market</div>
+        <div className='title'>First</div>
+        <div className='title'>Second</div>
+        <div className='title'>Third</div>
+      </div>
+        {Object.entries(data).map(item => <div className='row'>
+          <div className='title'>{item[0]}</div>
+          <div className='value'>{+item[1].first.toFixed(3)}</div>
+          <div className='value'>{+item[1].second.toFixed(3)}</div>
+          <div className='value'>{+item[1].third.toFixed(3)}</div>
         </div>)}
     </div>
     </>
